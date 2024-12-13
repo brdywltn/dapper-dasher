e2b1a8ea6b112b7b0177238439bd582685a8f9e7
Brody <brodywilton@hotmail.com>
2024-12-13 21:48:24 +0000

    Add velocity and jump on keypress - need to bring it back down

--- dasher.cpp
+++ dasher.cpp
@@ -6,17 +6,37 @@ int main() {
     const int windowHeight {450};
     const int windowWidth {800};
     const char windowTitle[] {"Dapper Dasher"};
-
+    // Initialise the window
     InitWindow(windowWidth, windowHeight, windowTitle);
 
+    // Rectangle dimensions
+    const int width {50};
+    const int height {80};
+    int posY {windowHeight - height};
+    int velocity {0};
 
     SetTargetFPS(60);
     while(!WindowShouldClose())
     {
+        // Start drawing
         BeginDrawing();
 
-        ClearBackground(RED);
+        if (IsKeyPressed(KEY_SPACE))
+        {
+            velocity -= 10;
+        }
 
+        posY += velocity;
+
+        DrawRectangle(windowWidth / 2, posY, width, height, BLUE);
+
+        
+
+        ClearBackground(WHITE);
+
+        // End drawing
         EndDrawing();
     }
+
+    CloseWindow();
 }
